4d0d4b636366e63addb993d1e91716b08d6aa651
Михаил <whoyuster@gmail.com>
2024-07-07 00:12:11 +0300

    Проверочный коммит

--- base/MultipleOfSeven.java
+++ base/MultipleOfSeven.java
@@ -2,6 +2,7 @@ public class MultipleOfSeven {
 
     public static void main(String[] args) {
         System.out.println("7 14 21 28 35 42 49 56 63 70 77 84 91 98");
+        System.out.println("Проверка Проверка Проверка");
     }
 
 }
